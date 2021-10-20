71c5ba6cd1bd0dd63053d399ad5b15874456f462
KristarDen <denis216051@gmail.com>
2021-10-20 22:08:30 +0300

    file get added

--- FirstProject/index.js
+++ FirstProject/index.js
@@ -2,25 +2,40 @@
 
 //подключение модуля
 const http = require( "http" ) ;
+var path = require('path')
 const HTTP_PORT  = 88;
+const WWW_ROOT = "./FirstProject/www";
+
+
+var fs = require('fs');
+
+    
+
 
 // серверная функция
 function serverFunction( request, response) {
     console.log( request.method + " " + request.url );
     
-    const url = request.url.substring(1) ;
-   /* if( url == '' || url == 'hello.js') {
-        response.statusCode = 200;
-        response.setHeader( 'Content-Type', 'text/html' );
-        response.end( "<h1>Server works<h1/>" ); //~getWriter().print
-    } else {
-        response.statusCode = 404;
-        response.setHeader( 'Content-Type', 'text/html' );
-        response.end( "<h1>Not Found<h1/>" ); //~getWriter().print
-    } */
+    let path =  WWW_ROOT + request.url;
 
-    response.statusCode = 200;
-    response.setHeader( 'Content-Type', ['text/html', 'charset=utf-8' ]);
+    console.log(path);
+    if( fs.existsSync( path ) ){
+
+        switch( getExtension(path) ){
+            case "css":
+                sendFile ( path, response, "css");
+                return;
+                break;
+
+            case "js":
+                sendFile ( path, response, "js");
+                return;
+                break;
+        }
+    }
+
+
+    const url = request.url.substring(1) ;
 
     switch (url) {
         case 'hello' :
@@ -32,13 +47,13 @@ function serverFunction( request, response) {
             break;
 
         case '' :
-            response.end( "<h1>Home, sweet Home<h1/>" );
+            
+            sendHTML('FirstProject/www/index.html', response);
             break;
 
         default : 
-            response.statusCode = 404;
-            response.end( "<h1>Не найдено<h1/>" );
-            break;
+        sendHTML('FirstProject/www/404.html', response, 404);
+        break;
     }
     
 }
@@ -53,3 +68,62 @@ server.listen(
         console.log("Listen start on " + HTTP_PORT + " port") ; 
     } 
 ) ;
+
+function sendFile( path, response, statusCode, fileType) {
+    fs.readFile( 
+        path, 
+        (err, data) => {
+
+            if(err) {
+                console.error( err );
+                return;
+            } 
+
+            response.statusCode = 200;
+            response.setHeader( 'Content-Type', 'text/' + fileType );
+            response.end(data);
+        } );
+}
+
+function sendHTML( path, response, statusCode) {
+
+    
+    if( typeof statusCode == 'undefined') statusCode = 200;
+    response.setHeader( 'Content-Type', 'text/html; charset=utf-8' );
+
+    if ( fs.existsSync(path) ) {//проверка наличия файла
+
+        const readStream = fs.createReadStream( path );
+       
+        readStream.pipe( response );
+
+    } else {// проверка наличия файла 404
+
+       if ( fs.existsSync('FirstProject/www/404.html') ){
+
+        const readStream = fs.createReadStream( path );
+        response.statusCode = 404;
+        
+        readStream.pipe( response );
+
+       } else {// отправка кода 418
+
+        response.statusCode = 418;
+        response.end( "<h1>Error, code 418 <h1/>" );
+
+       }
+
+    }
+    
+}
+
+
+function getExtension(filename) {
+    try{
+        let ext = path.extname(filename||'').split('.');
+        return ext[ext.length - 1];
+    } catch{
+        return;
+    }
+    
+}
